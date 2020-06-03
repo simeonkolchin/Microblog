a9ee6b0c9b91184dc31bee89da09e2a6cf50dcda
Симеон Колчин <rabotnik.p18@mail.ru>
2020-06-03 16:00:40 +0300

    dir

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+project_dir=os.path.join(basedir, 'app')
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
