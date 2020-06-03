f28f54f1bb0084c177adcdf9df58f4eab120d35d
Симеон Колчин <rabotnik.p18@mail.ru>
2020-06-03 15:56:17 +0300

    config, template_dir

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 25
+    TEMPLATES_DIR= os.path.join(project_dir, 'Templates')
