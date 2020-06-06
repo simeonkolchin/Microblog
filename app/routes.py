f1b016fdbce096183dffb15f11bb20c3c3d4005f
Симеон Колчин <rabotnik.p18@mail.ru>
2020-06-06 12:10:43 +0300

    index2

--- app/routes.py
+++ app/routes.py
@@ -209,3 +209,24 @@ def messenge():
     return render_template('messenge.html', title='Messenger', form=form,
                            posts=posts.items, next_url=next_url,
                            prev_url=prev_url)
+
+@app.route('/messenger', methods=['GET', 'POST'])
+@login_required
+def messenger():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('messenger'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('messenger', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('messenger', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('messenger.html', title='Message', form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
