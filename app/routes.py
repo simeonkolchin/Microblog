6f6081feb414dde4122163cf36c56c2c2e11dbe8
Симеон Колчин <rabotnik.p18@mail.ru>
2020-06-09 00:19:54 +0300

    login checkbox

--- app/routes.py
+++ app/routes.py
@@ -230,3 +230,24 @@ def messenger():
     return render_template('messenger.html', title='Message', form=form,
                            posts=posts.items, next_url=next_url,
                            prev_url=prev_url)
+
+@app.route('/history', methods=['GET', 'POST'])
+@login_required
+def history():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('history'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('history', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('history', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('history.html', title='History', form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
